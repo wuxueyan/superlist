5d79b09e6055ca24cdd06ab97d2efc54c48efcb2
Haiwera <982220546@qq.com>
2017-06-24 12:46:34 +0800

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -12,11 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get('http://127.0.0.1:8000')
 
 		self.assertIn('To-Do',self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do',hander_text)
+		self.assertIn('To-Do',header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
@@ -26,12 +26,12 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys('Buy peacock feathers')
 
-		inputbox.send_keys(keys.Enter)
+		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_elements_by_id('id_list_table')
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-				any(row.text == '1:Buy peacock feathers' for row in rows)
+				any(row.text == '1:Buy peacock feathers' for row in rows),"New to-do item did not appear in table"
 		)
 
 		self.fail('Finish the test!')
