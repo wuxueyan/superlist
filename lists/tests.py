21423cff0b977ec5763ff1ad23d9c7f5ed82bddf
Haiwera <982220546@qq.com>
2017-07-09 10:14:47 +0800

    commit two project not now

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.views import home_page
+from views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -17,3 +17,16 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(),expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+		self.assertIn('A new list item',response.content.decode())
+		expected_html = render_to_string(
+				'home.html',{'new_item_text':'A new list item'}
+				)
+		self.assertEqual(response.content.decode(),expected_html)
+
