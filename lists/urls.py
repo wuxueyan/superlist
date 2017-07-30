457e05fd584f216e91faf504727acfb37dcc175a
Haiwera <982220546@qq.com>
2017-07-30 21:21:53 +0800

    url in lists

--- lists/urls.py
+++ lists/urls.py
@@ -21,10 +21,8 @@ from lists.views import new_list
 from lists.views import add_item
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-	#url(r'^lists/(\d+)/$',view_list,name='view_list'),
-	#url(r'^lists/(\d+)/add_item$',add_item,name = 'add_item'),
-	#url(r'^lists/new$',new_list,name='new_list'),
-	
 	url(r'^$',home_page,name='home'),
-	url(r'^lists/',include('lists.urls')),
+	url(r'^(\d+)/$',view_list,name='view_list'),
+	url(r'^(\d+)/add_item$',add_item,name = 'add_item'),
+	url(r'^new$',new_list,name='new_list'),
 ]
